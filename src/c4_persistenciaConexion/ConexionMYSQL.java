f9fbc9913acd9968e67a6ff3755b1381881082cb
franzgupioc <franz.gupioc@janaq.com>
2016-09-29 17:33:48 +0100

    refactor de gitignore

--- src/c4_persistenciaConexion/ConexionMYSQL.java
+++ src/c4_persistenciaConexion/ConexionMYSQL.java
@@ -12,11 +12,10 @@ public class ConexionMYSQL extends GestorJDBC {
     @Override
     public void abrirConexion() throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
-        conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/almacenbd", "root", "sdsdf");
+        conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/almacenbd", "root", "");
         conexion.createStatement();
         if (conexion != null) {
             //JOptionPane.showMessageDialog(null, "Connecion exitosa");
         }
     }
-
 }
